8b4b858b3892a176622f5b4ebf5ad6912e0f30c9
ninesky00 <jiangzho@gmail.com>
2020-12-17 20:58:08 -0600

    remove unusable validation

--- app/models/application.rb
+++ app/models/application.rb
@@ -2,7 +2,7 @@ class Application < ApplicationRecord
   has_many :pet_applications
   has_many :pets, through: :pet_applications
   validates :name, :address, :city, :state, :zip, :description, presence: { message: "Field cannot be empty"}
-  validates :pets, :includsion: { in: %w(false), message: "pet must be adoptable" }
+
   enum status: [:in_progress, :pending, :accepted, :rejected]
   
 end
